237f3b64e631d2c3fe5a5ce459969efc2d567fad
nobeljb <nobeljulianbintang@gmail.com>
2025-03-07 20:22:11 +0700

    [RED] refine Test PaymentServiceImpl

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImplTest.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,16 +30,28 @@ class PaymentServiceImplTest {
 
     @BeforeEach
     void setUp() {
+        List<Product> products = new ArrayList<>();
+        products = new ArrayList<Product>();
+        Product product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(2);
+        products.add(product1);
+
         MockitoAnnotations.openMocks(this);
-        order = new Order("1", List.of(), 1708560000L, "John Doe", "WAITING_PAYMENT");
+        order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                products, 1708560000L, "Safira Sudrajat");
         paymentData = new HashMap<>();
     }
 
     @Test
     void testAddPaymentWithValidVoucher() {
         paymentData.put("voucherCode", "ESHOP1234ABC5678");
-        Payment payment = new Payment(order, "Voucher", paymentData);
-        when(paymentRepository.save(any(Payment.class))).thenReturn(payment);
+
+        when(paymentRepository.save(any(Payment.class))).thenAnswer(invocation -> {
+            Payment savedPayment = invocation.getArgument(0);
+            return savedPayment; // Return the modified object
+        });
 
         Payment result = paymentService.addPayment(order, "Voucher", paymentData);
         assertEquals("SUCCESS", result.getStatus());
@@ -46,8 +60,11 @@ class PaymentServiceImplTest {
     @Test
     void testAddPaymentWithInvalidVoucher() {
         paymentData.put("voucherCode", "INVALID12345678");
-        Payment payment = new Payment(order, "Voucher", paymentData);
-        when(paymentRepository.save(any(Payment.class))).thenReturn(payment);
+
+        when(paymentRepository.save(any(Payment.class))).thenAnswer(invocation -> {
+            Payment savedPayment = invocation.getArgument(0);
+            return savedPayment;
+        });
 
         Payment result = paymentService.addPayment(order, "Voucher", paymentData);
         assertEquals("REJECTED", result.getStatus());
