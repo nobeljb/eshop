cfd72e8c36d859ee583639cb9cf20ba65fcfe330
nobeljb <nobeljulianbintang@gmail.com>
2025-03-07 19:43:07 +0700

    [RED] change tests for Payment model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,9 +1,9 @@
 import id.ac.ui.cs.advprog.eshop.model.Order;
-import id.ac.ui.cs.advprog.eshop.model.Payment;
-import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,38 +11,58 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentTest {
+    private List<Product> products;
     private Order order;
     private Map<String, String> paymentData;
-    private Payment payment;
 
     @BeforeEach
     void setUp() {
-        order = new Order("1", List.of(), 1708560000L, "John Doe", OrderStatus.WAITING_PAYMENT.getValue());
+        this.products = new ArrayList<Product>();
+        Product product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(2);
+
+        this.products.add(product1);
+        order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, 1708560000L, "Safira Sudrajat");
         paymentData = new HashMap<>();
-        paymentData.put("voucherCode", "ESHOP1234ABC5678");
-        payment = new Payment(order, "Voucher", paymentData);
     }
 
     @Test
-    void testCreatePayment() {
+    void testCreatePaymentDefaultStatus() {
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment(order, "Voucher", paymentData);
+
         assertNotNull(payment.getId());
         assertEquals("Voucher", payment.getMethod());
         assertEquals("PENDING", payment.getStatus());
-        assertEquals("ESHOP1234ABC5678", payment.getPaymentData().get("voucherCode"));
         assertEquals(order, payment.getOrder());
     }
 
     @Test
-    void testSetStatusToSuccess() {
-        payment.setStatus("SUCCESS");
-        assertEquals("SUCCESS", payment.getStatus());
-        assertEquals("SUCCESS", order.getStatus());
+    void testCreatePaymentWithSuccessStatus() {
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment(order, "Voucher", paymentData);
+        payment.setStatus(PaymentStatus.SUCCESS.getValue());
+
+        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentWithInvalidStatus() {
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment(order, "Voucher", paymentData);
+
+        assertThrows(IllegalArgumentException.class, () -> payment.setStatus("MEOW"));
     }
 
     @Test
     void testSetStatusToRejected() {
-        payment.setStatus("REJECTED");
-        assertEquals("REJECTED", payment.getStatus());
-        assertEquals("FAILED", order.getStatus());
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment(order, "Voucher", paymentData);
+        payment.setStatus(PaymentStatus.REJECTED.getValue());
+
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 }
